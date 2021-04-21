276a2b2c4ac77ac309888bdc1abff66b78383784
kevin <kevinzhao0709@163.com>
2021-04-21 15:58:33 +0800

    1）新增add路由和它的逻辑实现。2）日志的初步使用。3）TODO标记疑问点。

--- src/main/java/com/rudecrab/ssm/controller/UserController.java
+++ src/main/java/com/rudecrab/ssm/controller/UserController.java
@@ -2,6 +2,8 @@ package com.rudecrab.ssm.controller;
 
 import com.rudecrab.ssm.entity.User;
 import com.rudecrab.ssm.service.UserService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,17 +11,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
 @RequestMapping(value = "user")
 public class UserController {
+
+    private static final Log logger = LogFactory.getLog(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @GetMapping("/getList")
     public String getList(Model model) {
         model.addAttribute("userList", userService.getAll());
+        // 返回一个网页
         return "index";
     }
 
@@ -27,5 +34,10 @@ public class UserController {
     @ResponseBody
     public List<User> getList() {
         return userService.getAll();
+    }
+
+    @GetMapping("/add")
+    public int addOne(User user) {
+        return userService.insert(user);
     }
 }
--- src/main/java/com/rudecrab/ssm/service/impl/UserServiceImpl.java
+++ src/main/java/com/rudecrab/ssm/service/impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.rudecrab.ssm.service.impl;
 
+import com.rudecrab.ssm.controller.UserController;
 import com.rudecrab.ssm.entity.User;
 import com.rudecrab.ssm.mapper.UserMapper;
 import com.rudecrab.ssm.service.UserService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +15,8 @@ import java.util.List;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class UserServiceImpl implements UserService {
+    private static final Log logger = LogFactory.getLog(UserController.class);
+
     @Autowired
     private UserMapper userMapper;
 
@@ -21,8 +26,24 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    // TODO 维持结构，将判断逻辑从controller移动到此函数
     public int insert(User user) {
-        return userMapper.add(user);
+        if (user.getName() == null || user.getId() == null || user.getPassword() == null)
+        {
+            logger.error("add fail params is null!");
+            return 0;
+        }
+
+        if ("".equals(user.getName())
+                || user.getId() == 0L
+                || "".equals(user.getPassword())) {
+            // TODO 日志如何格式化输出？ eg:logger.info("fail %s", user.getName()) or logger.info("fail ", user.getName())
+            logger.info(user);
+            return 0;
+        } else {
+            logger.info("add secuess!");
+            return userMapper.add(user);
+        }
     }
 }
 
